a0f38e1bb1d108c9d701ef9e286497b108f37188
AIE\s221308 <s221308@students.aie.edu.au>
2024-06-17 14:57:11 +0930

    started on the game manager and circle fish going to pull from B

--- Project_Files/Assets/Scripts/GameManager.cs
+++ Project_Files/Assets/Scripts/GameManager.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int score;
+    public float lifeTimer;
+
+    public float scoreTimer;
+
+    private bool gameStarted = false;
+
+    public Transform[] circleSpawners;
+
+    public GameObject circlerFish;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +23,34 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(gameStarted)
+        {
+            lifeTimer -= Time.deltaTime;
+
+            scoreTimer += Time.deltaTime;
+        }
+
+        if(lifeTimer <= 0)
+        {
+            ///end game here
+        }
+    }
+
+    public void StartSpawnCircler()
+    {
+        StartCoroutine(SpawnCircler());
+    }
+
+    public IEnumerator SpawnCircler()
+    {
+        yield return new WaitForSeconds(7);
+
+        Instantiate(circlerFish, circleSpawners[Random.Range(0, circleSpawners.Length)]);
+    }
+
+    public void StartGame()
+    {
+        gameStarted = true;
+        StartSpawnCircler();
     }
 }
