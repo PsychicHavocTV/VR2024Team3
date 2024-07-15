15727e1b6f5f8eb845069fc6d51143faf727ce8f
Liam <128756342+LiamGulliverAIE@users.noreply.github.com>
2024-07-15 14:34:32 +0930

    ploijhio

--- Project_Files/Assets/Scripts/PlinkoManager.cs
+++ Project_Files/Assets/Scripts/PlinkoManager.cs
@@ -17,7 +17,7 @@ public class PlinkoManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(wonBoxes.Count == 0)
+        if (!wonBoxes[0])
         {
             return;
         }
