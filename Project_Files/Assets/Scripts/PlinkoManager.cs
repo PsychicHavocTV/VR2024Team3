4b5a14267ae1cfb0f74661156f7e35c2d0ba39b0
AIE\s221308 <s221308@students.aie.edu.au>
2024-07-15 12:06:29 +0930

    fixed plinko win spamming

--- Project_Files/Assets/Scripts/PlinkoManager.cs
+++ Project_Files/Assets/Scripts/PlinkoManager.cs
@@ -24,6 +24,7 @@ public class PlinkoManager : MonoBehaviour
         if(wonBoxes.Count == scoreToWin)
         {
             GameManager.singleton.FinishCurrentGame();
+            Destroy(gameObject);
         }
     }
 
