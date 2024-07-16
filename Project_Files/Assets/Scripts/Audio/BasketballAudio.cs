d852cd59052b8dc9df1cdb0f54f3506a784e306c
AIE\s213209 <s213209@students.aie.edu.au>
2024-07-16 10:27:07 +0930

    added an audio folder, started some framework for audio

---
+++ Project_Files/Assets/Scripts/Audio/BasketballAudio.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketballAudio : MonoBehaviour
+{
+    [SerializeField] private AudioSource bbSource;
+    [SerializeField] private bool hasMultipleSounds;
+    public AudioClip[] sounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.ToLower() == "level" || other.tag.ToLower() == "floor")
+        {
+            if (hasMultipleSounds == true)
+            {
+                int soundChoice = 0;
+                soundChoice = Random.Range(0, sounds.Length);
+                Debug.Log("Sound Choice: " + soundChoice);
+                SoundManager.Instance.PlayFromSource(bbSource, sounds[soundChoice]);
+            }
+            else
+            {
+                SoundManager.Instance.PlayFromSource(bbSource, sounds[0]);
+            }
+        }
+    }
+
+}
