2b819043a0f9f039626b9f1e0908d2d43b4888f6
CameronAIE <s221308@students.aie.edu.au>
2024-07-17 11:38:33 +0930

    i think this will make the text look less clutterd

--- Project_Files/Assets/Scripts/GameManager.cs
+++ Project_Files/Assets/Scripts/GameManager.cs
@@ -88,7 +88,7 @@ public class GameManager : MonoBehaviour
 
             for (int i = 0; i < levelTimes.Count; i++)
             {
-                results += guantletScenes[i] + " time: " + levelTimes[i].ToString("0.00") + "\n";
+                results += guantletScenes[i] + " time: " + levelTimes[i].ToString("0.00") + "\n\n";
             }
 
             backBoardText.text = results;
